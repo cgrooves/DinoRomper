246f6a3c2f5cd2fe8a136a1e9d8d7596e843e394
Caleb Groves <caleb.j.groves@gmail.com>
2020-01-09 20:09:59 -0700

    Basic XZ character control

--- dev/DinoRomper_Godot/Character.gd
+++ dev/DinoRomper_Godot/Character.gd
@@ -1,8 +1,5 @@
 extends KinematicBody
 
-# constants
-const VEL_INPUT = 5
-
 # class variables
 var velocity = Vector3(0,0,0)
 
@@ -11,20 +8,25 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-func _input(event):
-	
-	if (event.is_action("ui_up")):
-			self.velocity.z = self.INPUT_VEL
-	elif (event.is_action("ui_down")):
-		velocity.z = -self.INPUT_VEL
-		
-	if (event.is_action("ui_right")):
-		self.velocity.x = self.INPUT_VEL
-	if (event.is_action("ui_left")):
-		self.velocity.x = -self.INPUT_VEL
-	
+func _process(delta):
 	# Update position
 	move_and_slide(velocity)
+
+# Handle inputs
+func _input(event):
+	
+	if (event.is_action_pressed("ui_up")):
+		velocity.z = 5
+	elif (event.is_action_pressed("ui_down")):
+		velocity.z = -5
+	elif (event.is_action_released("ui_up")
+			or event.is_action_released("ui_down")):
+		velocity.z = 0
+		
+	if (event.is_action_pressed("ui_right")):
+		velocity.x = -5
+	elif (event.is_action_pressed("ui_left")):
+		velocity.x = 5
+	elif (event.is_action_released("ui_left")
+			or event.is_action_released("ui_right")):
+		velocity.x = 0
